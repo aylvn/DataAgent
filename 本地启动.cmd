e858958303717f60b8b3a0724806725426acbe9b
aylvn <aylvn@sina.com>
2026-05-10 09:12:31 +0800

    配置变更

--- 本地启动.cmd
+++ 本地启动.cmd
@@ -4,6 +4,9 @@
 D:
 cd D:\IdeaProjects\sz-water\data-agent\data-agent-management
 chcp 65001
-$env:JAVA_HOME="C:\Program Files\Java\jdk-21"; $env:PATH="C:\Program Files\Java\jdk-21\bin;$env:PATH"; ../mvnw -U -s ../maven-settings.xml spring-boot:run
+set CONSOLE_CHARSET=GBK
+$env:JAVA_HOME="C:\Program Files\Java\jdk-21";
+$env:PATH="C:\Program Files\Java\jdk-21\bin;$env:PATH";
+../mvnw -U -s ../maven-settings.xml spring-boot:run
 
 
